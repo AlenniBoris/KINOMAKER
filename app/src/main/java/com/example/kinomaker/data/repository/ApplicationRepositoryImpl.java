77be7d15e180ade0af38b2183aa3290dcc39320a
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2024-12-03 16:08:23 +0300

    implemented repository functionality

--- app/src/main/java/com/example/kinomaker/data/repository/ApplicationRepositoryImpl.java
+++ app/src/main/java/com/example/kinomaker/data/repository/ApplicationRepositoryImpl.java
@@ -1,7 +1,14 @@
 package com.example.kinomaker.data.repository;
 
 import com.example.kinomaker.data.database.ApplicationDatabase;
+import com.example.kinomaker.domain.model.JobApplication;
+import com.example.kinomaker.domain.model.Movie;
+import com.example.kinomaker.domain.model.User;
 import com.example.kinomaker.domain.repository.ApplicationRepository;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -18,4 +25,84 @@ public class ApplicationRepositoryImpl implements ApplicationRepository {
         this.database = database;
     }
 
+    @Override
+    public void loginUser(
+            String email,
+            String password,
+            OnSuccessListener<DocumentSnapshot> onSuccessListener,
+            OnFailureListener onFailureListener
+    ) {
+       database.getDb().collection("users")
+               .document(email)
+               .get()
+               .addOnSuccessListener(documentSnapshot -> {
+                   if (documentSnapshot.exists()){
+                       String documentedPassword = documentSnapshot.getString("password");
+                       if (documentedPassword != null && documentedPassword.equals(password)){
+                           onSuccessListener.onSuccess(documentSnapshot);
+                       }else {
+                           onFailureListener.onFailure(new Exception("Wrong password"));
+                       }
+                   } else{
+                       onFailureListener.onFailure(new Exception("No such user. Registration is needed"));
+                   }
+               })
+               .addOnFailureListener(onFailureListener);
+    }
+
+    @Override
+    public void registerUser(
+            User user,
+            OnSuccessListener<Void> onSuccessListener,
+            OnFailureListener onFailureListener
+    ) {
+       database.getDb().collection("users")
+               .document(user.getEmail())
+               .set(user)
+               .addOnSuccessListener(onSuccessListener)
+               .addOnFailureListener(onFailureListener);
+    }
+
+    @Override
+    public void updateUserField(
+            String email,
+            String field,
+            Object value,
+            OnSuccessListener<Void> onSuccessListener,
+            OnFailureListener onFailureListener
+    ) {
+        database.getDb().collection("users")
+                .document(email)
+                .update(field, value)
+                .addOnSuccessListener(onSuccessListener)
+                .addOnFailureListener(onFailureListener);
+    }
+
+    @Override
+    public void addNewMovieToResume(
+            String email,
+            Movie movie,
+            OnSuccessListener<Void> onSuccessListener,
+            OnFailureListener onFailureListener
+    ) {
+        database.getDb().collection("users")
+                .document(email)
+                .update("resume.movies", FieldValue.arrayUnion(movie))
+                .addOnSuccessListener(onSuccessListener)
+                .addOnFailureListener(onFailureListener);
+    }
+
+    @Override
+    public void addNewJobApplication(
+            String email,
+            JobApplication jobApplication,
+            OnSuccessListener<Void> onSuccessListener,
+            OnFailureListener onFailureListener
+    ) {
+        database.getDb().collection("users")
+                .document(email)
+                .update("applications", FieldValue.arrayUnion(jobApplication))
+                .addOnSuccessListener(onSuccessListener)
+                .addOnFailureListener(onFailureListener);
+    }
 }
--- app/src/main/java/com/example/kinomaker/domain/repository/ApplicationRepository.java
+++ app/src/main/java/com/example/kinomaker/domain/repository/ApplicationRepository.java
@@ -1,7 +1,47 @@
 package com.example.kinomaker.domain.repository;
 
+import com.example.kinomaker.domain.model.JobApplication;
+import com.example.kinomaker.domain.model.Movie;
+import com.example.kinomaker.domain.model.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+
 public interface ApplicationRepository {
 
+    public void loginUser(
+            String email,
+            String password,
+            OnSuccessListener<DocumentSnapshot> onSuccessListener,
+            OnFailureListener onFailureListener
+            );
 
+    public void registerUser(
+            User user,
+            OnSuccessListener<Void> onSuccessListener,
+            OnFailureListener onFailureListener
+    );
+
+    public void updateUserField(
+            String email,
+            String field,
+            Object value,
+            OnSuccessListener<Void> onSuccessListener,
+            OnFailureListener onFailureListener
+    );
+
+    public void addNewMovieToResume(
+            String email,
+            Movie movie,
+            OnSuccessListener<Void> onSuccessListener,
+            OnFailureListener onFailureListener
+    );
+
+    public void addNewJobApplication(
+            String email,
+            JobApplication jobApplication,
+            OnSuccessListener<Void> onSuccessListener,
+            OnFailureListener onFailureListener
+    );
 
 }
